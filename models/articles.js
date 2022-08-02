74b32831737796fc65db11481ca67bf95489b92b
alexi <alexihampson8@gmail.com>
2022-08-02 10:06:38 +0100

    Completes Ticket 7

--- models/articles.js
+++ models/articles.js
@@ -5,7 +5,11 @@ exports.selectArticleById = async (id) => {
   const {
     rows: [row],
   } = await db.query(
-    "SELECT articles.article_id AS article_id, articles.author AS author, title, articles.body AS body, topic, articles.created_at AS created_at, articles.votes AS votes, COUNT(comment_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id=$1 GROUP BY articles.article_id;",
+    `SELECT articles.article_id AS article_id, articles.author AS author, title, articles.body AS body, topic, articles.created_at AS created_at, articles.votes AS votes, COUNT(comment_id) AS comment_count 
+    FROM articles 
+    LEFT JOIN comments ON articles.article_id = comments.article_id 
+    WHERE articles.article_id=$1 
+    GROUP BY articles.article_id;`,
     [id]
   );
 
