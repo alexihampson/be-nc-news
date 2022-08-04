9f4b4ab42bf186597c2d17f2aa73dc97f4a0a2e5
alexi <alexihampson8@gmail.com>
2022-08-04 11:39:09 +0100

    Completes Ticket 18

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,6 +2,7 @@ const {
   selectCommentsByArticleId,
   insertCommentByArticleId,
   removeCommentById,
+  updateCommentById,
 } = require("../models/comments");
 const { selectArticleById } = require("../models/articles");
 
@@ -34,6 +35,19 @@ exports.deleteCommentById = (req, res, next) => {
   removeCommentById(comment_id)
     .then(() => {
       res.status(204).send();
+    })
+    .catch(next);
+};
+
+exports.patchCommentById = (req, res, next) => {
+  const {
+    params: { comment_id },
+    body,
+  } = req;
+
+  updateCommentById(comment_id, body)
+    .then((comment) => {
+      res.status(200).send({ comment });
     })
     .catch(next);
 };
